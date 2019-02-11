a8c6d75aabc22959655e813b75c1ef2e4bcb799d
Travis Clarke <travis.m.clarke@gmail.com>
2019-02-11 12:56:50 -0800

    add multiple merge strategy options: REPLACE, ADDITIVE, and TYPESAFE

--- mergedeep/mergedeep.py
+++ mergedeep/mergedeep.py
@@ -1,5 +1,6 @@
 from collections.abc import Mapping
 from copy import deepcopy
+from enum import Enum
 from functools import reduce
 from typing import TypeVar, MutableMapping as Map
 
@@ -7,12 +8,58 @@ KT = TypeVar("KT")
 VT = TypeVar("VT")
 
 
-def merge(target: Map[KT, VT], *sources: Map[KT, VT]) -> Map[KT, VT]:
+class Strategy(Enum):
+    # Replace `target` item with one from `source` (default).
+    REPLACE = 0
+    # Combined `list` or `set` tpyes into one collection.
+    ADDITIVE = 1
+    # Raise `TypeError` when `target` and `source` types differ.
+    TYPESAFE = 2
+
+
+def _handle_merge_replace(target, source, key):
+    # If a key exists in both objects and the values are `different`, the value from the `source` object will be used.
+    target[key] = source[key]
+
+
+def _handle_merge_additive(target, source, key):
+    # List and Set values are combined into one long collection.
+    if isinstance(target[key], list) and isinstance(source[key], list):
+        # Extend target if both target and source are `list` type.
+        target[key].extend(source[key])
+    elif isinstance(target[key], set) and isinstance(source[key], set):
+        # Update target if both target and source are `set` type.
+        target[key].update(source[key])
+    else:
+        _handle_merge[Strategy.REPLACE](target, source, key)
+
+
+def _handle_merge_typesafe(target, source, key):
+    # Raise a TypeError if the target and source types differ.
+    if type(target[key]) is not type(source[key]):
+        raise TypeError(
+            f'target type: {type(target[key])} differs from source type: {type(source[key])} for key: "{key}"'
+        )
+    else:
+        _handle_merge[Strategy.REPLACE](target, source, key)
+
+
+_handle_merge = {
+    Strategy.REPLACE: _handle_merge_replace,
+    Strategy.ADDITIVE: _handle_merge_additive,
+    Strategy.TYPESAFE: _handle_merge_typesafe,
+}
+
+
+def merge(
+    target: Map[KT, VT], *sources: Map[KT, VT], strategy: Strategy = Strategy.REPLACE
+) -> Map[KT, VT]:
     """
     A deep merge function for 🐍.
 
     :param target: Map[KT, VT]:
     :param *sources: Map[KT, VT]:
+    :param strategy: Strategy (Default: Strategy.REPLACE):
     """
 
     def _deepmerge(target: Map[KT, VT], source: Map[KT, VT]):
@@ -31,8 +78,9 @@ def merge(target: Map[KT, VT], *sources: Map[KT, VT]) -> Map[KT, VT]:
                     # If a key exists in both objects and the values are `same`, the value from the `target` object will be used.
                     pass
                 else:
-                    # If a key exists in both objects and the values are `different`, the value from the `source` object will be used.
-                    target[key] = deepcopy(source[key])
+                    _handle_merge.get(strategy, Strategy.REPLACE)(
+                        target, deepcopy(source), key
+                    )
             else:
                 # If the key exists only in `source`, the value from the `source` object will be used.
                 target[key] = deepcopy(source[key])
