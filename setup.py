6f577bf29750c073f6764ffbea29ab6d6759a8d3
Travis Clarke <travis.m.clarke@gmail.com>
2020-04-18 20:39:18 -0700

    officially label support for python3.8

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
